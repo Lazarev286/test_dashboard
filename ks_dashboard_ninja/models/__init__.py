f3fb456a577658890a6a8a68d74ec06022c6e31e
Lazarev Matvei <lazarev280601@gmail.com>
2023-02-16 17:12:31 +0200

    add items

---
+++ ks_dashboard_ninja/models/__init__.py
@@ -0,0 +1,10 @@
+from . import lbs_dashboard
+from . import lbs_dashboard_child
+from . import lbs_dashboard_items
+# from . import ks_dashboard_ninja_items
+from . import ks_item_action
+from . import ks_dashboard_filters
+from . import ks_dashboard_templates
+from . import ks_odoo_base
+from . import ks_dn_to_do_item
+from . import ks_import_dashboard
